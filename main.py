da6a8a42e0496333029aba39435ab8790b81a391
Tanzhor_RIK <tanzhor4@gmail.com>
2024-03-17 01:23:03 +0700

    Капучино

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import sqlite3
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QTableWidgetItem
 
 
 class MyWidget(QMainWindow):
@@ -10,10 +9,10 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi("main.ui", self)
         self.connection = sqlite3.connect("coffee.sqlite")
-        self.setWindowTitle('Coffee')
-
+        self.setWindowTitle('🍵')
         self.show_result()
-        self.titles = None
+        self.red = Redactor()
+        self.pushButton.clicked.connect(self.open_r)
 
     def show_result(self):
         cursor = self.connection.cursor()
@@ -27,6 +26,59 @@ class MyWidget(QMainWindow):
         for i, elem in enumerate(result):
             for j, val in enumerate(elem):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+
+    def open_r(self):
+        self.hide()
+        self.red = Redactor()
+        self.red.show()
+
+
+class Redactor(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.connection = sqlite3.connect("coffee.sqlite")
+        self.setWindowTitle('🍵')
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton.clicked.connect(self.save_results)
+        self.pushButton_2.clicked.connect(self.new_coffee)
+        self.modified = {}
+        self.show_result()
+
+    def show_result(self):
+        cur = self.connection.cursor()
+        result = cur.execute("SELECT * FROM coffee").fetchall()
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+
+    def item_changed(self, item):
+        self.modified[self.titles[item.column()]] = item.text()
+
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            cur.execute("UPDATE coffee SET " + ", ".join(
+                [f"{key}='{self.modified.get(key)}'"
+                 for key in
+                 self.modified.keys()]) + f" WHERE ID = {self.modified['ID']}")
+            self.con.commit()
+            self.modified.clear()
+
+    def new_coffee(self):
+        cur = self.con.cursor()
+        cur.execute(
+            f"""INSERT INTO coffee VALUES({self.lineEdit_1.text()},
+                                            '{self.lineEdit_2.text()}',
+                                            '{self.lineEdit_3.text()}',
+                                            '{self.lineEdit_4.text()}',
+                                            '{self.lineEdit_5.text()}',
+                                            '{self.lineEdit_6.text()}',
+                                            '{self.lineEdit_7.text()}');""")
+        self.con.commit()
 
 
 if __name__ == '__main__':
