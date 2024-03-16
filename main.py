1e05b42b9c88f2cefbac46648b580be5cea175a4
Tanzhor_RIK <tanzhor4@gmail.com>
2024-03-16 16:18:22 +0700

    Эспрессо

---
+++ main.py
@@ -0,0 +1,36 @@
+import sqlite3
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("main.ui", self)
+        self.connection = sqlite3.connect("coffee.sqlite")
+        self.setWindowTitle('Coffee')
+
+        self.show_result()
+        self.titles = None
+
+    def show_result(self):
+        cursor = self.connection.cursor()
+        result = cursor.execute("SELECT * FROM coffee").fetchall()
+
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.tableWidget.setHorizontalHeaderLabels(
+            ['ID', 'название сорта', 'степень обжарки', 'молотый/в зернах',
+             'описание вкуса', 'цена', 'объем упаковки'])
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
